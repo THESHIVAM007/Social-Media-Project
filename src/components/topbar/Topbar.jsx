c32b1f24918864e90ef465005d0b617afada50bf
Shivam Choudhary <96073230+THESHIVAM007@users.noreply.github.com>
2023-01-11 14:37:18 +0530

    topbar-completed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./topbar.css"
-import{Search,Person} from "@material-ui/icons"
+import{Search,Person,Chat, Notifications} from "@mui/icons-material"
 const Topbar = () => {
   return (
-    <div className='topbarContainer'>Topbar
+    <div className='topbarContainer'>
 <div className="topbarLeft">
   <span className="logo">Social Call</span>
 </div>
 <div className="topbarCenter">
   <div className="searchbar">
-<Search/>
+<Search className='searchIcon'/>
 <input placeholder="Search for friend,post or video" className="searchInput" />
   </div>
 </div>
@@ -25,8 +25,20 @@ const Topbar = () => {
         1
       </span>
     </div>
+    <div className="topbarIconItem">
+      <Chat/>
+      <span className="topbarIconBadge">
+        2
+      </span>
+    </div>
+    <div className="topbarIconItem">
+      <Notifications/>
+      <span className="topbarIconBadge">
+        1
+      </span>
+    </div>
   </div>
-
+<img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="" className="topbarImg" />
 </div>
 
 
