d86a5be195e56f274722af20294139517f81739a
Shivam Choudhary <96073230+THESHIVAM007@users.noreply.github.com>
2023-01-20 22:24:22 +0530

    sidebar-components

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,50 @@
+import { RssFeed } from '@mui/icons-material'
 import React from 'react'
 import "./sidebar.css"
 const Sidebar = () => {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>Sidebar
+    <div className="sidebarWrapper">
+      <ul className="sidebarlist">
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+        <li className="sidebarListItem">
+          <RssFeed className='sidebarIcon'/>
+          <span className="sidebarListItemText"></span>
+        </li>
+      </ul>
+    </div>
+    </div>
   )
 }
 
